79fe2995a5c566257a1bc36cf963ed13e37a2def
atakan yontar <atakanyontar@gmail.com>
2022-02-06 02:38:37 +0300

    Set files to use template engine

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs')
 
 const app = express();
 
-app.get('/', (req,res) => {
-	const photo = {
-		id:1,
-		name:"Photo Name",
-		description:"Photo Description"
-	}
-	res.send(photo)
-})
+//TEMPLATE ENGINE
+app.set("view engine","ejs");
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
+app.get('/add', (req, res) => {
+	res.render('add')
+  });
+  app.get('/about', (req, res) => {
+	res.render('about')
+  });
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server has started running on port ${port}`);
