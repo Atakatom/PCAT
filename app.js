aa91be008c1b5a4229d8d024a18c58f449cb0e58
atakan yontar <atakanyontar@gmail.com>
2022-02-11 17:32:56 +0300

    Configured middlewares for dynamic content

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const path = require('path');
-const ejs = require('ejs')
+const ejs = require('ejs');
+const Photo = require('./models/Photo')
 
 const app = express();
 
 //TEMPLATE ENGINE
-app.set("view engine","ejs");
+app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
 });
 app.get('/add', (req, res) => {
-	res.render('add')
-  });
-  app.get('/about', (req, res) => {
-	res.render('about')
-  });
+  res.render('add');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.post('/photos', async (req,res) => {
+  Photo.create(req.body);
+  res.redirect('/')
+})
 
 const port = 3000;
 app.listen(port, () => {
