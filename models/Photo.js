501aeb2db361c4d994da5cefe3b79393880e5106
atakan yontar <atakanyontar@gmail.com>
2022-02-11 17:34:46 +0300

    Initialized the schema for mongodb

---
+++ models/Photo.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//create Schema
+const PhotoSchema = new Schema({
+  title: String,
+  description: String,
+  image: String,
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Photo = mongoose.model('Photo', PhotoSchema);
+
+module.exports = Photo
